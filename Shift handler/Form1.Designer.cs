33acbfa8e03a37d3054270854749a1aa7937dd7a
Thabiso K Phetla <phetlathabiso@gmail.com>
2016-10-24 10:12:30 +0200

    truck company asked for an application to hundle shifts

---
+++ Shift handler/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Question4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstLocations = new System.Windows.Forms.ListBox();
+            this.btnStartShift = new System.Windows.Forms.Button();
+            this.lstShiftPlan = new System.Windows.Forms.ListBox();
+            this.btnAddTrip = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstLocations
+            // 
+            this.lstLocations.FormattingEnabled = true;
+            this.lstLocations.Items.AddRange(new object[] {
+            "Capetown:1200:12",
+            "Pretoria:100:3",
+            "Middelburg:160:5",
+            "Boksburg:200:4",
+            "Bloemfontein:400:6",
+            "Durban:800:9",
+            "Kokstad:1600:10"});
+            this.lstLocations.Location = new System.Drawing.Point(12, 21);
+            this.lstLocations.Name = "lstLocations";
+            this.lstLocations.Size = new System.Drawing.Size(150, 173);
+            this.lstLocations.TabIndex = 0;
+            // 
+            // btnStartShift
+            // 
+            this.btnStartShift.Location = new System.Drawing.Point(12, 200);
+            this.btnStartShift.Name = "btnStartShift";
+            this.btnStartShift.Size = new System.Drawing.Size(150, 23);
+            this.btnStartShift.TabIndex = 1;
+            this.btnStartShift.Text = "Start Shift";
+            this.btnStartShift.UseVisualStyleBackColor = true;
+            this.btnStartShift.Click += new System.EventHandler(this.btnStartShift_Click);
+            // 
+            // lstShiftPlan
+            // 
+            this.lstShiftPlan.FormattingEnabled = true;
+            this.lstShiftPlan.Location = new System.Drawing.Point(194, 21);
+            this.lstShiftPlan.Name = "lstShiftPlan";
+            this.lstShiftPlan.Size = new System.Drawing.Size(616, 173);
+            this.lstShiftPlan.TabIndex = 2;
+            // 
+            // btnAddTrip
+            // 
+            this.btnAddTrip.Enabled = false;
+            this.btnAddTrip.Location = new System.Drawing.Point(12, 229);
+            this.btnAddTrip.Name = "btnAddTrip";
+            this.btnAddTrip.Size = new System.Drawing.Size(150, 23);
+            this.btnAddTrip.TabIndex = 3;
+            this.btnAddTrip.Text = "Add Trip";
+            this.btnAddTrip.UseVisualStyleBackColor = true;
+            this.btnAddTrip.Click += new System.EventHandler(this.btnAddTrip_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 386);
+            this.Controls.Add(this.btnAddTrip);
+            this.Controls.Add(this.lstShiftPlan);
+            this.Controls.Add(this.btnStartShift);
+            this.Controls.Add(this.lstLocations);
+            this.Name = "Form1";
+            this.Text = "Question 4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstLocations;
+        private System.Windows.Forms.Button btnStartShift;
+        private System.Windows.Forms.ListBox lstShiftPlan;
+        private System.Windows.Forms.Button btnAddTrip;
+    }
+}
+
---
+++ Shift handler/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question4
+{
+    public partial class Form1 : Form
+    {
+        DateTime shifStart;
+        DateTime shiftEnd;
+        DateTime shifCounter;
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartShift_Click(object sender, EventArgs e)
+        {
+            /* Q 4.1 (5)
+             * 
+             * Makhulu ULU transport services makes deliveries to all major towns and cities in South Africa. 
+             * 
+             * They have asked you to develop an application that will ensure their drivers are not over worked. 
+             * 
+             * To ensure driver safety they need to ensure that a driver does not drive more then 36 hours in one shift.
+             * 
+             * When btnStartShift is clicked write the code that will:
+             * 
+             * Set the class variable named shifStart equal to the current date and time.
+             * 
+             * Set the class variable shiftEnd equal to shifStart with 36 hours added.
+             * 
+             * clear the list box named lstShiftPlan 
+             * 
+             * Add both the shiftStart and shiftEnd values to the list box named lstShiftPlan in the following format:
+             * 
+             *     New shift started at {{shifStart}} shift will end at  {{shifEnd}}.
+             * 
+             * You must use the standard date time format string "g" (General date/time pattern) as the format when displaying the shiftStart and shiftEnd
+             * 
+             */
+
+            btnAddTrip.Enabled = true; //Provided
+
+            shifStart = DateTime.Now; //1
+            //shifStart = System.DateTime;
+          shiftEnd = shifStart.AddHours(36); //1
+           // shiftEnd = DateTime.Parse(shifStart + 36);
+            lstShiftPlan.Items.Clear(); //1
+
+            lstShiftPlan.Items.Add(String.Format("New shift started at {0} shift will end at  {1}.", shifStart.ToString("dd/MM/yyyy hh:mm:ss's'"), shiftEnd.ToString("dd/MM/yyyy hh:mm:ss's'"))); //2 - Correctly adding start and stop time
+
+        }
+
+        private void btnAddTrip_Click(object sender, EventArgs e)
+        {
+            /*Q 4.2 (15)
+             * 
+             * The Listbox named lstLocations contains a number of town where deliveries is made to. The data for each town is in the following format:
+             * 
+             * Each line of the list box  represents one delivery location in the following format:
+             * 
+             * TOWN,DISTANCE,TIME
+             * 
+             * Example:
+             * 
+             * Capetown:1200:12
+             * 
+             * Fields:
+             *  TOWN: The name of the town or city
+             *  DISTANCE: The distance to the city and back again.
+             *  TIME: The time it takes in hours to drive to the city and back again.
+             * 
+             * When btnAddTrip is clicked write the code that will:
+             * 
+             * Add the selected line from lstLocations to lstShiftPlan in the following format:
+             * 
+             *  New trip added for driver to {{TOWN}} trip will take  {{TIME}} hours.
+             * 
+             * Example:
+             * 
+             *  New trip added for driver to Capetown trip will take 12 hours.
+             *  
+             * The application should also indicate how the trip will affect the time allocated for the drivers shift. 
+             * 
+             * This can be accomplished by adding the amount of time the trip will take to the shift start time as every new trip is added. You can use class variable shifCounter for this purpose.
+             * 
+             * Make sure that the shifCounter is set equal to the  shifStart time when only the first trip is added. As more trips is added the shifCounter should be incremented with the time it takes to make the selected trip. 
+             * 
+             * The following line should be added to lstShiftPlan 
+             * 
+             * Selected trip will end at {{shifCounter}} shift must end at {{SHIFT END TIME}} driver can work {{HOURS}} more hours.
+             * 
+             * This should be repeated for every new trip added to the shift. As new trips are added the shift time calculation above should be updated accordingly.
+             * 
+             * If at any time the drivers shift exceeds 36 hours the following line should be added to the list box named lstShiftPlan:
+             * 
+             * Above trip is not possible as it exceeds the 36 hours allocated time please remove the trip. 
+             * 
+             * You must use the standard date time format string "g" (General date/time pattern) as the format when displaying any date time variables.
+             * 
+             */
+
+            //2 - Shift counter should on = shifStart if the first trip is added 
+            if(shifCounter == DateTime.MinValue)
+            {
+                shifCounter = shifStart;
+            }
+
+            // 3 - Extract the duration and the location for selected trip (Must be correct for 3 marks)
+            string line = (string)lstLocations.Items[lstLocations.SelectedIndex];
+           
+            string location = line.Split(':')[0];
+            int time = Convert.ToInt32(line.Split(':')[2]);
+
+            //1 - Add the trip to lstShiftPlan
+            //lstShiftPlan.Items.Add(String.Format("New trip added for driver to {0} trip will take  {1} hours.", location,time));
+            lstShiftPlan.Items.AddRange(new string[] { "New trip added for driver to " + location + " trip will take  " +  time.ToString() + " hours."});
+
+            //2 - Calculate the {{SHIFT START TIME or SHIFT START TIME WITH PREVIOS TRIPS ADDED  + TRIM DURATION TIME(TIME)}}
+            shifCounter = shifCounter.AddHours(time);
+
+            //2 Calculate how many hours the driver can still work (any method acceptable)
+            TimeSpan hoursRem = shiftEnd.Subtract(shifCounter);
+
+            //2 - Show information(notice Total hours)
+            lstShiftPlan.Items.Add(String.Format("Selected trip will end at {0} shift must end at {1} driver can work {2} more hours.", shifCounter.ToString("g"), shiftEnd.ToString("g"), hoursRem.TotalHours.ToString() ));
+
+            //3 - Check if time is exceeded
+            if(hoursRem.TotalHours < 0)
+            {
+                lstShiftPlan.Items.Add("Above trip is not possible as it exceeds the 36 hours allocated time please remove the trip.");
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
